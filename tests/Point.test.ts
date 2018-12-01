73dc63ec4232c183167e9fca5d90fffb739a40fa
Michal Ozogan <michal@ozogan.eu>
2018-12-01 16:29:57 +0100

    Add test

--- tests/Point.test.ts
+++ tests/Point.test.ts
@@ -5,4 +5,12 @@ describe('Test Point model', () => {
         const point = new Point({ hr: 5 });
         expect(point.toObject()).toHaveProperty('hr', 5);
     });
+
+    it('clones object on set property', () => {
+        const point = new Point({ hr: 5 });
+        expect(point.getHeartRate()).toEqual(5);
+        const point2 = point.setHeartRate(6);
+        expect(point.getHeartRate()).toEqual(5);
+        expect(point2.getHeartRate()).toEqual(6);
+    });
 });
